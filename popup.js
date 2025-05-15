8d4b037921e782fa4765c75785d2af35e2ba19b0
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2025-05-15 00:02:31 -0300

    Adds i18n support and new translations
    Implements internationalization (i18n) to support multiple languages.
    
    Adds message files for German, French, Indonesian, Malay, Portuguese (Brazilian), and Turkish.
    
    Updates the manifest file to include the default locale.
    
    Modifies the popup HTML to use data-i18n attributes for translation.
    
    Adds a new i18n.js file to handle the translation of the popup content.

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("startButton").addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "removeRepostedVideos" });
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.getAttribute("data-i18n");
+    const message = chrome.i18n.getMessage(key);
+    if (message) element.innerHTML = message;
   });
+
+  const startButton = document.getElementById("startButton");
+  if (startButton) {
+    startButton.addEventListener("click", function () {
+      chrome.runtime.sendMessage({ action: "removeRepostedVideos" });
+    });
+  }
 });
